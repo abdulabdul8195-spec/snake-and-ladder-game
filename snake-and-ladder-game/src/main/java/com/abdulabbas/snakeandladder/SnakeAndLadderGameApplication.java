0207d4a52a3a789996ea7a781076c72144a8e222
Abdul Abbas <abdulabdul8195@gmail.com>
2026-01-22 15:57:44 +0530

    New: A new project for snake and ladder game DAY-01 22-01-2026

---
+++ snake-and-ladder-game/src/main/java/com/abdulabbas/snakeandladder/SnakeAndLadderGameApplication.java
@@ -0,0 +1,13 @@
+package com.abdulabbas.snakeandladder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeAndLadderGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeAndLadderGameApplication.class, args);
+	}
+
+}
